bd7061db9720258e36d6c14194491dcc8ebad6bf
akwasi kusi-appiah <akappiah@gmail.com>
2022-11-09 17:07:34 +0000

    updated myData.js

---
+++ jobfunction.js
@@ -0,0 +1,31 @@
+//Get JSON File
+
+async function getData(){
+    const result= await fetch('./starter-code/data');
+
+    try {
+        if (result.ok){
+            const rsData= await result.json();
+            return JSON.parse(rsData);
+        }
+
+    }catch(error){
+        console.log (error);
+    }
+
+}
+
+const myData=getData();
+
+
+
+// const myJSON=require(./starter-code/assets/)
+
+
+// Load Items from JSON file and Append to Grid
+
+// const myJobRecords=JSON.parse();
+
+function loadGridItems(){
+    console.log(myData);
+}
